2a27528574ddd7a0df6c052b624b4038490b4e4d
Semanur Eserler <78433748+Senures@users.noreply.github.com>
2022-12-22 12:16:09 +0300

    auth added

--- lib/di.dart
+++ lib/di.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_example/shared/service/auth_service.dart';
 import 'package:firebase_example/shared/service/firebase_service.dart';
 import 'package:get/get.dart';
 
 class DenpendencyInjection {
   static init() async {
     await Get.putAsync(() => FirebaseService().init());
+    await Get.putAsync(() => AuthService().init());
   }
 }
