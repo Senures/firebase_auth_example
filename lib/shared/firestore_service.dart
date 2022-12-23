aebc195d9d34694529529e089fb440551c9df109
Semanur Eserler <78433748+Senures@users.noreply.github.com>
2022-12-23 14:05:43 +0300

    collection ekleme

---
+++ lib/shared/firestore_service.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class FireStoreService extends GetxService {
+  createTodoCollection() async {
+    //koleksiyon içine alt koleksiyon açmak
+    var to_do = FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .collection("to_do");
+
+    await to_do.add({
+      "category_name": categoryname!.text,
+      "task_color": todoColor, //burda firebase ekledik
+      "total_task": "",
+    });
+  }
+}
